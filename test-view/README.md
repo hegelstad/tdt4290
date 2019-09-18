7f8cd3416b64ba6d87bb2047747565e9ff7dcf09
Jonas Bakken <jonasxb@gmail.com>
2019-09-18 12:42:11 +0200

    Add core example usage to test-view

--- test-view/README.md
+++ test-view/README.md
@@ -1,7 +1,8 @@
 # Test view
 
 To start:
-* Follow instructions for `view`
+* Follow instructions for `view` and `core`
 * `yarn` to install dependencies
 * `yarn link "view"` to use the view package
-* `yarn start`
+* `yarn link "core"` to use the core package
+* `REACT_APP_TOKEN=xxxxxx REACT_APP_ORG=xxxxxx yarn start`
--- test-view/src/App.js
+++ test-view/src/App.js
@@ -1,11 +1,38 @@
 import React from "react";
 
 import GremlinView from "view";
+import { initialize, followBranch, executeQuery } from "core";
 
-const App = () => (
-  <div>
-    <GremlinView />
-  </div>
-);
+const config = {
+  org: process.env.REACT_APP_ORG,
+  token: process.env.REACT_APP_TOKEN,
+  apiURL: "/api/graph-search"
+};
+
+const App = () => {
+  console.log(config);
+  const test_query = async () => {
+    console.log("Create base query object:");
+    let query = await initialize(config);
+    console.log(query);
+
+    console.log("Select all Departments:");
+    query = await followBranch(query, { type: "label", value: "Department" });
+    console.log(query);
+
+    console.log("Follow all Belongs To relations:");
+    query = await followBranch(query, { type: "edge", value: "Belongs To" });
+    console.log(query);
+
+    console.log("Execute query");
+    console.log(await executeQuery(query));
+  };
+  test_query();
+  return (
+    <div>
+      <GremlinView />
+    </div>
+  );
+};
 
 export default App;
