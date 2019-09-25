83b0b8ca85b74c7a9b6ca88becb9a96060e3bc5b
olafapl <olaf.liadal@gmail.com>
2019-09-25 10:45:30 +0200

    Add styled-components, "fix" dependencies

---
+++ view/src/styles/theme.js
@@ -0,0 +1,33 @@
+const grayDark = "#373737";
+const gray = "#d6d9e1";
+const white = "#fff";
+const offWhite = "#f7f8fa";
+const purple = "#670867";
+
+const theme = {
+  fontFamily: "sans-serif",
+  colors: {
+    foreground: grayDark,
+    background: white,
+    highlight: purple,
+    grayDark,
+    gray,
+    white,
+    offWhite,
+    purple
+  },
+  spacing: [
+    "0px",
+    "4px",
+    "8px",
+    "16px",
+    "32px",
+    "64px",
+    "128px",
+    "256px",
+    "512px"
+  ],
+  roundRadius: "2px"
+};
+
+export default theme;
