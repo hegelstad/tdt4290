1cfaf91ee1bae104d5d3c4192ca81f168cbe23cc
MiriamFi <mfinjord@gmail.com>
2019-10-18 15:23:09 +0200

    Made input field and drop down menu into styled components.

--- view/src/components/FilterView.tsx
+++ view/src/components/FilterView.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 const FilterView = ({
   properties,
@@ -10,12 +11,14 @@ const FilterView = ({
   const [filterKey, setFilterKey] = useState("");
   const [fieldValue, setFieldValue] = useState("");
 
-  const handleDropDownChange = (event: any) => {
+  const handleDropDownChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
     setFilterKey(event.target.value);
     console.log("FilterKey: " + filterKey);
   };
 
-  const handleInputChange = (event: any) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFieldValue(event.target.value);
   };
 
@@ -32,13 +35,41 @@ const FilterView = ({
     return false;
   };
 
-  console.log("properties: " + properties);
+  // styled components
+
+  const ValueInput = styled.input.attrs(() => ({
+    type: "text",
+    defaultValue: fieldValue,
+    onInput: handleInputChange
+  }))`
+  padding: 2px
+    margin-bottom: 8px
+    margin-left: 10px;
+  `;
+
+  const FieldSelect = styled.select.attrs(() => ({
+    defaultValue: filterKey,
+    onChange: handleDropDownChange
+  }))`
+  padding: 2px
+    margin-bottom: 8px
+    margin-left: 18px;
+  `;
+
+  const FilterButton = styled.button.attrs(() => ({
+    onClick: () => handleSubmit(callback)
+  }))`
+  padding: 2px 5px
+    margin-bottom: 8px
+    margin-left: 10px;
+  `;
+
   return (
     <div>
       {componentHasFilter(properties) ? (
         <div>
           <h3>Filter</h3>
-          <select value={filterKey} onChange={handleDropDownChange}>
+          <FieldSelect>
             <option key={"default"} value="">
               --Choose field--
             </option>
@@ -47,14 +78,9 @@ const FilterView = ({
                 {prop}
               </option>
             ))}
-          </select>
-          <input
-            type="text"
-            value={fieldValue}
-            onChange={handleInputChange}
-            placeholder="Select a value..."
-          />
-          <button onClick={() => handleSubmit(callback)}>Filter</button>
+          </FieldSelect>
+          <ValueInput placeholder="Select a value..." autoFocus />
+          <FilterButton>Filter</FilterButton>
         </div>
       ) : null}
     </div>
