3649df2495ca2c03dd37c28b965d9995e58a710f
MiriamFi <mfinjord@gmail.com>
2019-10-29 09:59:25 +0100

    Small fix up in import reference

--- packages/view/src/components/FilterView.tsx
+++ packages/view/src/components/FilterView.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { FilterCallbackType } from "../types/types";
+import { FilterCallbackType } from "../types";
 
 const FilterView = ({
   properties,
